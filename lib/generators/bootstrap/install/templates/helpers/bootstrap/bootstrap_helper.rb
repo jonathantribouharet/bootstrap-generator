4198b8a0b11dbd3b3163db194630d1514d84bc01
Jonathan <jonathan@asus.(none)>
2012-06-18 12:12:53 +0200

    Navbar mobile

--- lib/generators/bootstrap/install/templates/helpers/bootstrap/bootstrap_helper.rb
+++ lib/generators/bootstrap/install/templates/helpers/bootstrap/bootstrap_helper.rb
@@ -4,7 +4,12 @@ module Bootstrap::BootstrapHelper
 		output = '
 		<div class="navbar navbar-fixed-top">
 			<div class="navbar-inner">
-			<div class="container">'
+			<div class="container">
+				<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </a>'			'
 				output += link_to(title, root_path, :class => 'brand')
 				output +='
 				<div class="nav-collapse">

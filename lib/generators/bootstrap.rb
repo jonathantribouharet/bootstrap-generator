1a47f00168f936ed82ac2ff38d88676b7f2ecd79
jonathan <jonathan@asus.(none)>
2012-07-27 14:39:47 +0200

    remove bootstrap to assets precompile

--- lib/generators/bootstrap.rb
+++ lib/generators/bootstrap.rb
@@ -1,11 +1,11 @@
 require 'rails/generators/base'
 
 module Bootstrap
-	module Rails
-		class Railtie < ::Rails::Railtie
-			initializer "configure assets of bootstrap-generator", :group => :all do |app|
-				app.config.assets.precompile += %w( bootstrap.js bootstrap.css )
-			end
-		end
-  end
+	# module Rails
+		# class Railtie < ::Rails::Railtie
+			# initializer "configure assets of bootstrap-generator", :group => :all do |app|
+				# app.config.assets.precompile += %w( bootstrap.js bootstrap.css )
+			# end
+		# end
+  # end
 end
